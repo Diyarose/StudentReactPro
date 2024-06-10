dd9297073bcb8533fbf3ac56f40863390d396cf2
Diyarose <dhiyarosedavis@gmail.com>
2024-06-10 20:34:51 +0530

    updates done

--- src/components/AddStudent.jsx
+++ src/components/AddStudent.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const AddStudent = () => {
+    const [data,setData]=useState(
+        {
+            
+                "name":"",
+                "admNo":"",
+                "college":"",
+                "dob":"",
+                "email":"",
+            
+        }
+    )
+    const inputHandler=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+    }
   return (
     <div>
         <Navbar/>
@@ -12,42 +29,30 @@ const AddStudent = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Name</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Admno</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" name='admNo' value={data.admNo} onChange={inputHandler} />
                         </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">RollNo</label>
-                            <input type="text" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-control">Parent Name</label>
-                            <input type="text" className="form-control" />
-                        </div>
+                       
+                       
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">College</label>
-                            <textarea name="" id="" className="form-control"></textarea>
+                            <textarea name="" id="" className="form-control" name='college' value={data.college} onChange={inputHandler}></textarea>
                         </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-6 col-xxl-6">
+                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">DOB</label>
-                            <input type="date" name="" id="" className="form-control" />
+                            <input type="date" name="" id="" className="form-control" name='dob' value={data.dob} onChange={inputHandler} />
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-6 col-xxl-6">
                             <label htmlFor="" className="form-label">Email</label>
-                            <input type="email" name="" id="" className="form-control" />
+                            <input type="email" name="" id="" className="form-control" name='email' value={data.email} onChange={inputHandler}/>
                         </div>
+                       
+                        
                         <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Password</label>
-                            <input type="password" name="" id="" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-6 col-xxl-6">
-                            <label htmlFor="" className="form-label">Confirm Password</label>
-                            <input type="password" name="" id="" className="form-control" />
-                        </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-12 col-xl-6 col-xxl-6">
-                            <button className="btn btn-success">Register</button>
+                            <button className="btn btn-success" onClick={readValue}>Register</button>
                         </div>
                     </div>
                 </div>
--- src/components/ViewallStudent.jsx
+++ src/components/ViewallStudent.jsx
@@ -1,114 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const ViewallStudent = () => {
+    const [data, changeData] = useState(
+        [{
+            "name": "Dhiya",
+            "admNo": "12445",
+            "college": "FISAT",
+            "dob": "15-04-2002",
+            "email": "dhiya@gmail.com",
+        }
+        ]
+    )
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <div className="container">
-            <h1><center><i>STUDENT LIST</i></center></h1>
-                <div className="row g-3">
-                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xx-12">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <div className="row">
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card">
-                                    <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?size=626&ext=jpg&ga=GA1.1.1222169770.1717632000&semt=sph" height="250px" class="card-img-top" alt="..."></img>
-                                    <div class="card-body">
-                                        <h5 class="card-title">Student</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                        <a href="#" class="btn btn-primary">Izabela</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card">
-                                    <img src="https://img.freepik.com/premium-photo/teenager-student-girl-yellow-pointing-finger-side_1368-40175.jpg" class="card-img-top" height="250px" alt="..."></img>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Student</h5>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Marina</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card">
-                                    <img src="https://parspng.com/wp-content/uploads/2022/12/studentpng.parspng.com-9.png" height="250px" class="card-img-top" alt="..."></img>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Student</h5>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Rahul</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card">
-                                    <img src="https://img.freepik.com/premium-photo/young-asian-indian-student-with-glasses-backpack-holds-book-shows-thumbs-up_928503-89.jpg" height="250px" class="card-img-top" alt="..."></img>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Student</h5>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Ram</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card">
-                                    <img src="https://w7.pngwing.com/pngs/615/209/png-transparent-graphy-student-students-class-photography-people-thumbnail.png" height="250px" class="card-img-top" alt="..."></img>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Student</h5>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Jennifer</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card">
-                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_dSh0irVcbzQKLQhAP0EJI0nlqauauddvYz1v0sLnCO67oiAZrJGnmRtrE3Iwz-Q-NHs&usqp=CAU" height="250px" class="card-img-top" alt="..."></img>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Student</h5>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Meritta</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card">
-                                    <img src="https://www.shutterstock.com/image-photo/portrait-smiling-young-college-student-260nw-1192615495.jpg" height="250px" class="card-img-top" alt="..."></img>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Student</h5>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Rix</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card">
-                                    <img src="https://www.keg.com/hubfs/johannes-1.png" height="250px" class="card-img-top" alt="..."></img>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Student</h5>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Seree</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card">
-                                    <img src="https://img.freepik.com/free-photo/front-view-male-student-wearing-black-backpack-holding-copybooks-smiling-blue-wall_140725-42653.jpg" height="250px" class="card-img-top" alt="..."></img>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Student</h5>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Dennis</a>
-                                        </div>
-                                </div>
-                            </div>
-                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div class="card">
-                                    <img src="https://www.avanse.com/viewPagesAssets/img/repayment-laon-calculator.png" height="250px" class="card-img-top" alt="..."></img>
-                                        <div class="card-body">
-                                            <h5 class="card-title">Student</h5>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Rose</a>
-                                        </div>
-                                </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Name</th>
+                                            <th scope="col">Admno</th>
+                                            <th scope="col">College</th>
+                                            <th scope="col">DOB</th>
+                                            <th scope="col">Email</th>
+                                           
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                       {data.map(
+                                        (value,index)=>{
+                                            return  <tr>
+                                            <th scope="row">{value.name}</th>
+                                            <td>{value.admNo}</td>
+                                            <td>{value.college}</td>
+                                            <td>{value.dob}</td>
+                                            <td>{value.email}</td>
+                                        </tr>
+                                        }
+                                       )}
+                                        
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>

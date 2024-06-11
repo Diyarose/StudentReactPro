ebcfa2ba49c9cfaed8941797434191a91eb5ef91
Diyarose <dhiyarosedavis@gmail.com>
2024-06-11 09:10:01 +0530

    updates done fully

--- src/components/DeleteStudent.jsx
+++ src/components/DeleteStudent.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const DeleteStudent = () => {
+    const [data,setData]=useState(
+        {
+            
+                "name":"",
+              
+            
+        }
+    )
+    const inputHandler=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+    }
   return (
     <div>
+        
         <Navbar/>
         <div className="container">
         <h1><center><i>REMOVE STUDENT DETAILS</i></center></h1>
             <div className="row g-3">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                     <label htmlFor="" className="form-label">Admno</label>
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler}/>
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button className="btn btn-danger">Delete</button>
+                    <button className="btn btn-danger" onClick={readValue}>Delete</button>
                 </div>
             </div>
         </div>
--- src/components/SearchStudent.jsx
+++ src/components/SearchStudent.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const SearchStudent = () => {
+    const [data,setData]=useState(
+        {
+            
+                "name":"",
+              
+            
+        }
+    )
+    const inputHandler=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(data)
+    }
   return (
     <div>
         <Navbar/>
@@ -9,11 +23,11 @@ const SearchStudent = () => {
         <h1><center><i>SEARCH STUDENT</i></center></h1>
             <div className="row g-3">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <label htmlFor="" className="form-control">Name</label>
-                    <input type="text" className="form-control" />
+                    <label htmlFor="" className="form-label">Name</label>
+                    <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler} />
                 </div>
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <button className="btn btn-success">Search</button>
+                    <button className="btn btn-success" onClick={readValue}>Search</button>
                 </div>
             </div>
         </div>
